c02b34f4ecfb45a83de83e0895e468e53a7f686e
unknown <wf.nelu@yandex.ru>
2020-11-23 20:20:05 +0100

    use react hooks

---
+++ markdown-react/src/App.class.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import './App.css';
+import { sampleText } from './sampleText';
+import marked from 'marked';
+
+class App extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      content: sampleText
+    }
+
+    this.handleUpdate = this.handleUpdate.bind( this );
+    this.getMarkdownText = this.getMarkdownText.bind( this );
+  }
+
+  componentDidMount() {
+    const data = localStorage.getItem( 'markdownData' );
+
+    if( data ) {
+      this.setState({ content: data });
+    } else {
+      this.setState({ content: sampleText });
+    }
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem( 'markdownData', this.state.content );
+  }
+
+  handleUpdate = ( event ) => {
+    const content = event.target.value;
+    this.setState({ content })
+  }
+
+  getMarkdownText = ( text ) => {
+    const __html = marked( text, { sanitize: true });
+    return { __html };
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <textarea 
+              className="markdown__textarea" 
+              onChange={this.handleUpdate}
+              value={this.state.content}
+            />
+          </div>
+          <div className="col-md-6">
+            <div className="markdown__content">
+              <p dangerouslySetInnerHTML={this.getMarkdownText(this.state.content)}></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
--- markdown-react/src/App.js
+++ markdown-react/src/App.js
@@ -1,63 +1,57 @@
-import React, { Component } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './App.css';
 import { sampleText } from './sampleText';
 import marked from 'marked';
 
-class App extends Component {
-  constructor( props ) {
-    super( props );
-    this.state = {
-      content: sampleText
+const App = ( props ) => {
+    const [ content, setContent ] = useState('');
+    const mounted = useRef();
+
+    useEffect( () => {
+      if (!mounted.current) {
+        // do componentDidMount logic
+        const data = localStorage.getItem( 'markdownData' );
+  
+        if ( data ) {
+          setContent( data );
+        } else {
+          setContent( sampleText );
+        }
+        mounted.current = true;
+      } else {
+        // do componentDidUpdate logic
+        localStorage.setItem( 'markdownData', content );
+      }
+    }, [content] );
+
+    const handleUpdate = ( event ) => {
+      const value = event.target.value;
+      setContent( value );
     }
 
-    this.handleUpdate = this.handleUpdate.bind( this );
-    this.getMarkdownText = this.getMarkdownText.bind( this );
-  }
-
-  componentDidMount() {
-    const data = localStorage.getItem( 'markdownData' );
-
-    if( data ) {
-      this.setState({ content: data });
-    } else {
-      this.setState({ content: sampleText });
+    const getMarkdownText = ( text ) => {
+      const __html = marked( text, { sanitize: true });
+      return { __html };
     }
-  }
 
-  componentDidUpdate() {
-    localStorage.setItem( 'markdownData', this.state.content );
-  }
-
-  handleUpdate = ( event ) => {
-    const content = event.target.value;
-    this.setState({ content })
-  }
-
-  getMarkdownText = ( text ) => {
-    const __html = marked( text, { sanitize: true });
-    return { __html };
-  }
-
-  render() {
     return(
       <div className="container">
         <div className="row">
           <div className="col-md-6">
             <textarea 
               className="markdown__textarea" 
-              onChange={this.handleUpdate}
-              value={this.state.content}
+              onChange={handleUpdate}
+              value={content}
             />
           </div>
           <div className="col-md-6">
             <div className="markdown__content">
-              <p dangerouslySetInnerHTML={this.getMarkdownText(this.state.content)}></p>
+              <p dangerouslySetInnerHTML={getMarkdownText(content)}></p>
             </div>
           </div>
         </div>
       </div>
-    )
-  }
+    );
 }
 
 export default App;
