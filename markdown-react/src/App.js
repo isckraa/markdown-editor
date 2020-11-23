af73d093b0628d8d8fa536ef86e59027b55dfd8a
unknown <wf.nelu@yandex.ru>
2020-11-23 18:24:14 +0100

    add markdown editor

--- markdown-react/src/App.js
+++ markdown-react/src/App.js
@@ -1,26 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import sampleText from './sampleText';
+import { sampleText } from './sampleText';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor( props ) {
+    super( props );
+    this.state = {
+      content: sampleText
+    }
+
+    this.handleUpdate = this.handleUpdate.bind( this );
+    this.getMarkdownText = this.getMarkdownText.bind( this );
+  }
+
+  handleUpdate = ( event ) => {
+    const content = event.target.value;
+    this.setState({ content })
+  }
+
+  getMarkdownText = ( text ) => {
+    const __html = marked( text, { sanitize: true });
+    return { __html };
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <textarea 
+              className="markdown__textarea" 
+              onChange={this.handleUpdate}
+              value={this.state.content}
+            />
+          </div>
+          <div className="col-md-6">
+            <div className="markdown__content">
+              <p dangerouslySetInnerHTML={this.getMarkdownText(this.state.content)}></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
